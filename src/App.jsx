3478bb6fa16e6ff54e2a4ad69e280b08557e88e0
Navjot <navjotnagra13@gmail.com>
2023-03-29 19:37:44 -0300

    navjot portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Header from "./sections/header/Header";
 import Navbar from "./sections/navbar/Navbar";
 import About from "./sections/about/About";
-import Services from "./sections/services/Services";
 import Portfolio from "./sections/portfolio/Portfolio";
-import Testimonials from "./sections/testimonials/Testimonials";
-import FAQs from "./sections/faqs/FAQs";
 import Contact from "./sections/contact/Contact";
 import Footer from "./sections/footer/Footer";
-import FloatingNav from "./sections/floating-nav/FloatingNav";
 
 const App = () => {
     return (
@@ -15,13 +11,9 @@ const App = () => {
             <Navbar />
             <Header />
             <About />
-            {/* <Services /> */}
             <Portfolio />
-            {/* <Testimonials /> */}
-            {/* <FAQs /> */}
             <Contact />
             <Footer />
-            {/* <FloatingNav /> */}
         </main>
     )
 }
