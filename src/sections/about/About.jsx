cfcee466c2e079c2b33f765f25d6a1b623ca549b
Navjot <navjotnagra13@gmail.com>
2023-03-29 19:48:50 -0300

    navjot portfolio

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__left">
                     <div className="about__portrait">
-                        <img src={Navjot} alt="About Image" />
+                        <img src={Navjot} alt="About" />
                     </div>
                 </div>
                 <div className="about__right">
